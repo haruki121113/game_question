698a3e0578749f59251c688e2b1e46936a9aa998
haruki121113 <satoharu11131920203@gmail.com>
2022-10-23 12:25:06 +0900

    一覧表示機能＋デザイン調整

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -18,6 +18,10 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def show
+    @question = Question.find(params[:id])
+  end
+
   private
   def question_params
     params.require(:question).permit(:title, :content).merge(user_id: current_user.id)
